e3713be6161317fe9b30f573500cde0129a70ba8
sdmelen <143933748+sdmelen@users.noreply.github.com>
2023-09-04 13:40:04 +0300

    Update main.c
    edit code

--- sh21_p_3_day/arithmetic_FIX/main.c
+++ sh21_p_3_day/arithmetic_FIX/main.c
@@ -1,30 +1,22 @@
-#include <stdio.h>
-#include <conio.h>
-
-int main()
-{
+int main() {
     int num_1, num_2;
     int a;
     a = scanf("%d%d", &num_1, &num_2);
-    if(a == 2)
-    {
-        int summ, minus, multiply, division;
+    int ch = getchar();
+    if (a == 2 && ((char)ch == '\n' || (char)ch == ' ' || ch == EOF)) {  // (char)ch == '\n' === ch == 10
+        int summ, minus, multiply;
         summ = num_1 + num_2;
         minus = num_1 - num_2;
         multiply = num_1 * num_2;
         if (num_2 == 0)
-            printf("n/a\n");
-        else
-        {
+            printf("n/a");
+        else {
+            int division;
             division = num_1 / num_2;
             printf("%d %d %d %d\n", summ, minus, multiply, division);
         }
+    } else {
+        printf("n/a");
     }
-    else
-    {
-        printf("n/a\n");
-    }
-    getch();
     return 0;
 }
-
